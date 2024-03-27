1a80127861c777dead1088a7c23011ce143a8119
GuilhermeEroSoft <Guilherme.EroSoft@hotmail.com>
2024-03-27 15:19:28 -0300

    Correções

--- next.js
+++ next.js
@@ -5,68 +5,57 @@ module.exports = {
     jest: true,
   },
   extends: [
-    "standard ",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:prettier/recommended",
+    'standard',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
   ],
-  parser: "@typescript-eslint/parser",
+  parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaFeatures: {
-      jsx: true,
+      jsx: true
     },
-    ecmaVersion: "latest",
-    sourceType: "module",
+    ecmaVersion: 'latest',
+    sourceType: 'module'
   },
-  plugins: ["jsx-a11y", "@typescript-eslint"],
+  plugins: [
+    'jsx-a11y',
+    '@typescript-eslint'
+  ],
   rules: {
     "no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
     "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
     "no-console": ["warn", { allow: ["error"] }],
     "react/jsx-props-no-spreading": "warn",
-    "@typescript-eslint/naming-convention": [
-      "error",
+    'prettier/prettier': ["error", {
+      'printWidth': 80,
+      'tabWidth': 2,
+      'singleQuote': true,
+      'trailingComma': 'all',
+      'arrowParens': 'always',
+      'semi': true,
+      'endOfLine': 'auto',
+      'singleAttributePerLine': true
+    }],
+    'jsx-a11y/alt-text': [
+      'warn',
       {
-        selector: "typeAlias",
-        format: ["PascalCase"],
-      },
-      {
-        selector: "interface",
-        format: ["PascalCase"],
-      },
-    ],
-    "prettier/prettier": [
-      "error",
-      {
-        arrowParens: "always",
-        endOfLine: "auto",
-        printWidth: 80,
-        semi: true,
-        singleAttributePerLine: true,
-        singleQuote: true,
-        tabWidth: 2,
-        trailingComma: "all",
+        elements: ['img'],
+        img: ['Image'],
       },
     ],
-    "jsx-a11y/alt-text": [
-      "warn",
-      {
-        elements: ["img"],
-        img: ["Image"],
-      },
-    ],
-    "jsx-a11y/aria-props": "warn",
-    "jsx-a11y/aria-proptypes": "warn",
-    "jsx-a11y/aria-unsupported-elements": "warn",
-    "jsx-a11y/role-has-required-aria-props": "warn",
-    "jsx-a11y/role-supports-aria-props": "warn",
-    "react/no-unknown-property": "error",
+    'jsx-a11y/aria-props': 'warn',
+    'jsx-a11y/aria-proptypes': 'warn',
+    'jsx-a11y/aria-unsupported-elements': 'warn',
+    'jsx-a11y/role-has-required-aria-props': 'warn',
+    'jsx-a11y/role-supports-aria-props': 'warn',
+    'react/no-unknown-property': 'error',
   },
   settings: {
     react: {
-      version: "detect",
+      version: 'detect',
     },
-    "import/parsers": {
-      [require.resolve("@typescript-eslint/parser")]: [".ts", ".tsx", ".d.ts"],
+    'import/parsers': {
+      [require.resolve('@typescript-eslint/parser')]: ['.ts', '.tsx', '.d.ts'],
     },
-  },
-};
+  }
+}
