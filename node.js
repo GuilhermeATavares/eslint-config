e450da928d7084cf01adfe26c70975e6db21974f
Guilherme Almeida Tavares <74673320+GuilhermeATavares@users.noreply.github.com>
2024-04-02 15:34:20 -0300

    Update node.js

--- node.js
+++ node.js
@@ -3,49 +3,29 @@ module.exports = {
     es2021: true,
     node: true,
   },
-  extends: [
-    "standard ",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:prettier/recommended",
-  ],
-  parser: "@typescript-eslint/parser",
+  extends: ['standard', 'plugin:@typescript-eslint/recommended', 'plugin:prettier/recommended'],
+  parser: '@typescript-eslint/parser',
   parserOptions: {
-    ecmaVersion: "latest",
-    sourceType: "module",
+    ecmaVersion: 'latest',
+    sourceType: 'module',
   },
-  plugins: ["@typescript-eslint"],
+  plugins: ['@typescript-eslint'],
   rules: {
-    "no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
-    "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
-    "no-console": ["warn", { allow: ["error"] }],
-    "@typescript-eslint/naming-convention": [
-      "error",
+    'prettier/prettier': [
+      'error',
       {
-        selector: "typeAlias",
-        format: ["PascalCase"],
-      },
-      {
-        selector: "interface",
-        format: ["PascalCase"],
-      },
-    ],
-    "prettier/prettier": [
-      "error",
-      {
-        arrowParens: "always",
-        endOfLine: "auto",
         printWidth: 80,
-        semi: true,
-        singleAttributePerLine: true,
-        singleQuote: true,
         tabWidth: 2,
-        trailingComma: "all",
+        singleQuote: true,
+        trailingComma: 'all',
+        arrowParens: 'always',
+        semi: false,
       },
     ],
   },
   settings: {
-    "import/parsers": {
-      [require.resolve("@typescript-eslint/parser")]: [".ts", ".tsx", ".d.ts"],
+    'import/parsers': {
+      [require.resolve('@typescript-eslint/parser')]: ['.ts', '.tsx', '.d.ts'],
     },
   },
-};
+}
